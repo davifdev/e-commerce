20f57bc8eb70588bd1a1711e0ca0b2625458e2c2
Davi Fernandes <davi.fer159@gmail.com>
2025-12-01 12:51:37 -0300

    chore: add setup firebase

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0A7yFBVbky7rzomtG4ICMKldhxqBJYnc",
+  authDomain: "club-clothing-6cafd.firebaseapp.com",
+  projectId: "club-clothing-6cafd",
+  storageBucket: "club-clothing-6cafd.firebasestorage.app",
+  messagingSenderId: "787474028528",
+  appId: "1:787474028528:web:ac0ad0f14980d24d776f35",
+};
+
+export const app = initializeApp(firebaseConfig);
