4a6b8bb2be6f14eb0c4c426f584fa8b4536dbf2d
Davi Fernandes <davi.fer159@gmail.com>
2025-12-02 18:12:55 -0300

    feat: fetch categories in firestore and add converter

---
+++ src/converters/firestore-converters.ts
@@ -0,0 +1,25 @@
+import type {
+  DocumentData,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+} from "firebase/firestore";
+import type { Category } from "../types/category-type";
+
+export const categoryConverter = {
+  toFirestore(category: Category): DocumentData {
+    return { ...category };
+  },
+  fromFirestore(
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions
+  ): Category {
+    const data = snapshot.data(options);
+    return {
+      id: data.id,
+      displayName: data.displayName,
+      name: data.name,
+      imageUrl: data.imageUrl,
+      products: data.products,
+    };
+  },
+};
